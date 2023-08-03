51dce88021d265b0aa42b4e4e0eca351dfd8ae8a
Sarah Hudson <78184189+sarahhcodes@users.noreply.github.com>
2023-08-03 12:14:55 -0700

    Updated code to generate 16x16 grid of squares

--- script.js
+++ script.js
@@ -1,9 +1,7 @@
 const container = document.querySelector('.container');
-console.log(container);
 
-for (let i = 0; i < 9; i++) {
+for (let i = 0; i < 256; i++) {
     let block = document.createElement('div');
     block.className = 'block';
-    console.log(block);
     container.appendChild(block);
 }
