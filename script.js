6adeb0156fd87e953341c08284926f59731b9f31
Sarah Hudson <78184189+sarahhcodes@users.noreply.github.com>
2023-08-04 12:12:38 -0700

    Added hover effect to blocks
    Also started working on reset button

--- script.js
+++ script.js
@@ -1,7 +1,14 @@
 const container = document.querySelector('.container');
+const button = document.querySelector('button');
 
 for (let i = 0; i < 256; i++) {
     let block = document.createElement('div');
     block.className = 'block';
     container.appendChild(block);
 }
+
+const blocks = document.querySelectorAll('.block');
+
+blocks.forEach(block => block.addEventListener('mouseover', () => {
+    block.classList.add('blockHover');
+}));
