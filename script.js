b843fd034541433b984cc91cc80fb6158ddd0e5b
Sarah Hudson <78184189+sarahhcodes@users.noreply.github.com>
2023-08-04 12:32:47 -0700

    Added button that adjusts number of blocks
    Also moved the following actions to their own functions:
    -drawing the grid
    -adding the hover effect to the blocks

--- script.js
+++ script.js
@@ -1,14 +1,39 @@
+/* TO DO
+- make size of the grid variable (i.e. 16x16)
+- add style to button
+- add gradient effect to hover (CSS variables?)
+*/
+
 const container = document.querySelector('.container');
 const button = document.querySelector('button');
+let numberOfBlocks = 256;
 
-for (let i = 0; i < 256; i++) {
-    let block = document.createElement('div');
-    block.className = 'block';
-    container.appendChild(block);
-}
+button.addEventListener('click', () => {
+    numberOfBlocks = prompt("How many blocks?", 256);
+    drawGrid();
+    playEAS();
+});
 
-const blocks = document.querySelectorAll('.block');
+console.log(numberOfBlocks);
+drawGrid();
 
-blocks.forEach(block => block.addEventListener('mouseover', () => {
-    block.classList.add('blockHover');
-}));
+function drawGrid() {
+    container.replaceChildren(); // clears grid
+
+        for (let i = 0; i < numberOfBlocks; i++) {
+            let block = document.createElement('div');
+            block.className = 'block';
+           container.appendChild(block);
+        };
+};
+
+// function that adds the hover effect to the blocks
+function playEAS() {
+    const blocks = document.querySelectorAll('.block');
+
+    blocks.forEach(block => block.addEventListener('mouseover', () => {
+        block.classList.add('blockHover');
+    }));
+};
+
+playEAS(); // ensures that the page loads with Etch-A-Sketch playable
